895c6cd056a4df81d92b57b31b24a46a2ac2fb98
TheGoodDude22 <jamesisbell@yahoo.com>
2021-03-13 00:54:42 -0600

    Changed the bot ready message.

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers for ${client.users.cache.size} users`);
+    console.log(`Ready on ${client.guilds.cache.size} servers for ${client.users.cache.size} users`);
 
     client.user.setActivity(client.config.discord.activity);
 };
