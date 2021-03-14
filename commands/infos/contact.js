2c0bae88bc0e7531d7615771d367e7f126d2d30c
TheGoodDude22 <jamesisbell@yahoo.com>
2021-03-14 00:29:30 -0600

    Cleaned up code massively.

--- commands/infos/contact.js
+++ commands/infos/contact.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { strike } = require('ffmpeg-static');
 
 module.exports = {
     name: 'contact',
@@ -30,154 +31,59 @@ module.exports = {
             .setColor('#0099ff');
 
         message.channel.send(contactEmbed).then((message) => {
-            message.react("🇦").then(() => {
-                message.react("🇧").then(() => {
-                    message.react("🇨").then(() => {
-                        message.react("🇩").then(() => {
-                            message.react("🇪").then(() => {
-                                message.react(client.emotes.off).then(() => {
-                                    
-                                    const filter = (reaction, user) => {
-                                        return ['🇦', '🇧', '🇨', '🇩', '🇪', `fail`].includes(reaction.emoji.name) && user.id;
-                                    };
-                                    
-                                    message.awaitReactions(filter, { max:1 }).then(collected => {
-                                    
-                                        const reaction = collected.first();
-                                        
-                                        if (reaction.emoji.name === '🇦') {
-                                    
-                                            message.channel.send(infoEmbed).then(() => {
-                                                
-                                                const filter = m => m.author.id === ogMessage.author.id
-                                                
-                                                message.channel.awaitMessages(filter, { max: 1, })
-                                                .then(async (collected) => {
-                                                    if (collected.first().content.toLowerCase() === 'cancel') {
-                                                        message.channel.send(cancelEmbed)
-                                                    } else {
-                                                        let embed1 = new Discord.MessageEmbed()
-                                                            .setColor('0x0099ff')
-                                                            .addField(`Discord Suggestion:`, `${collected.first().content}`)
-                                                            .setFooter(`${ogMessage.author.tag}`)
-                                                            .setTimestamp();
+            message.react('🇦')
+            .then(() => message.react('🇧'))
+            .then(() => message.react('🇨'))
+            .then(() => message.react('🇩'))
+            .then(() => message.react('🇪'))
+            .then(() => message.react(client.emotes.off))
+            .then(() => {
+                const offEmote = client.emotes.off.split(':')[1];
 
-                                                        const channel = await client.channels.fetch(client.setChannels.discordSuggestions)
-                                                        channel.send(embed1)
-                                    
-                                                        message.channel.send(successEmbed)
-                                                    } 
-                                                })
-                                            })
-                                        }
-                                        if (reaction.emoji.name === '🇧') {
-                                    
-                                            message.channel.send(infoEmbed).then(() => {
-                                                
-                                                const filter = m => m.author.id === ogMessage.author.id
-                                    
-                                                message.channel.awaitMessages(filter, { max: 1, })
-                                                .then(async (collected) => {
-                                                    if (collected.first().content.toLowerCase() === 'cancel') {
-                                                        message.channel.send(cancelEmbed)
-                                                    } else {
-                                                        let embed2 = new Discord.MessageEmbed()
-                                                            .setColor('0x0099ff')
-                                                            .addField(`Bot Suggestion:`, `${collected.first().content}`)
-                                                            .setFooter(`${ogMessage.author.tag}`)
-                                                            .setTimestamp();
-                                    
-                                                        const channel = await client.channels.fetch(client.setChannels.botSuggestions)
-                                                        channel.send(embed2)
-                                    
-                                                        message.channel.send(successEmbed)
-                                                    }
-                                                })
-                                            })
-                                        }
-                                        if (reaction.emoji.name === '🇨') {
-                                    
-                                            message.channel.send(infoEmbed).then(() => {
-                                                
-                                                const filter = m => m.author.id === ogMessage.author.id
-                                    
-                                                message.channel.awaitMessages(filter, { max: 1, })
-                                                .then(async (collected) => {
-                                                    if (collected.first().content.toLowerCase() === 'cancel') {
-                                                        message.channel.send(cancelEmbed)
-                                                    } else {
-                                                        let embed2 = new Discord.MessageEmbed()
-                                                            .setColor('0x0099ff')
-                                                            .addField(`Bot Bug Report:`, `${collected.first().content}`)
-                                                            .setFooter(`${ogMessage.author.tag}`)
-                                                            .setTimestamp();
-                                    
-                                                        const channel = await client.channels.fetch(client.setChannels.botBugReports)
-                                                        channel.send(embed2)
-                                    
-                                                        message.channel.send(successEmbed)
-                                                    }
-                                                })
-                                            })
-                                        }
-                                        if (reaction.emoji.name === '🇩') {
-                                    
-                                            message.channel.send(infoEmbed).then(() => {
-                                                
-                                                const filter = m => m.author.id === ogMessage.author.id
-                                    
-                                                message.channel.awaitMessages(filter, { max: 1, })
-                                                .then(async (collected) => {
-                                                    if (collected.first().content.toLowerCase() === 'cancel') {
-                                                        message.channel.send(cancelEmbed)
-                                                    } else {
-                                                        let embed2 = new Discord.MessageEmbed()
-                                                            .setColor('0x0099ff')
-                                                            .addField(`User Report:`, `${collected.first().content}`)
-                                                            .setFooter(`${ogMessage.author.tag}`)
-                                                            .setTimestamp();
-                                    
-                                                        const channel = await client.channels.fetch(client.setChannels.userReports)
-                                                        channel.send(embed2)
-                                    
-                                                        message.channel.send(successEmbed)
-                                                    }
-                                                })
-                                            })
-                                        }
-                                        if (reaction.emoji.name === '🇪') {
-                                    
-                                            message.channel.send(infoEmbed).then(() => {
-                                                
-                                                const filter = m => m.author.id === ogMessage.author.id
-                                    
-                                                message.channel.awaitMessages(filter, { max: 1, })
-                                                .then(async (collected) => {
-                                                    if (collected.first().content.toLowerCase() === 'cancel') {
-                                                        message.channel.send(cancelEmbed)
-                                                    } else {
-                                                        let embed2 = new Discord.MessageEmbed()
-                                                            .setColor('0x0099ff')
-                                                            .addField(`Other Contact:`, `${collected.first().content}`)
-                                                            .setFooter(`${ogMessage.author.tag}`)
-                                                            .setTimestamp();
-                                    
-                                                        const channel = await client.channels.fetch(client.setChannels.otherContact)
-                                                        channel.send(embed2)
-                                    
-                                                        message.channel.send(successEmbed)
-                                                    }
-                                                })
-                                            })
-                                        }
-                                        if (reaction.emoji.name === 'fail') {
-                                            message.channel.send(cancelEmbed)
-                                        }
-                                    })
-                                })
+                const filter = (reaction, user) => {
+                    return ['🇦', '🇧', '🇨', '🇩', '🇪', offEmote].includes(reaction.emoji.name) && user.id;
+                };
+        
+                message.awaitReactions(filter, { max:1 })
+                    .then(collected => {                     
+                    const reaction = collected.first();
+
+                    if (reaction.emoji.name === offEmote) {
+                        message.channel.send(cancelEmbed)
+                    } else {
+                        message.channel.send(infoEmbed).then(() => {
+                            
+                            const filter = m => m.author.id === ogMessage.author.id
+                            
+                            message.channel.awaitMessages(filter, { max: 1, })
+                            .then(async (collected) => {
+                                if (collected.first().content.toLowerCase() === 'cancel') {
+                                    message.channel.send(cancelEmbed)
+                                } else {
+                                    let embed1 = new Discord.MessageEmbed()
+                                        .setColor('0x0099ff')
+                                        .addField(`Discord Suggestion:`, `${collected.first().content}`)
+                                        .setFooter(`${ogMessage.author.tag}`)
+                                        .setTimestamp();
+        
+                                    if (reaction.emoji.name === '🇦') {
+                                        var channel = await client.channels.fetch(client.setChannels.discordSuggestions);
+                                    } else if (reaction.emoji.name === '🇧') {
+                                        var channel = await client.channels.fetch(client.setChannels.botSuggestions);
+                                    } else if (reaction.emoji.name === '🇨') {
+                                        var channel = await client.channels.fetch(client.setChannels.botBugReports);
+                                    } else if (reaction.emoji.name === '🇩') {
+                                        var channel = await client.channels.fetch(client.setChannels.userReports);
+                                    } else if (reaction.emoji.name === '🇪') {
+                                        var channel = await client.channels.fetch(client.setChannels.otherContact);
+                                    }
+                                    channel.send(embed1)
+                
+                                    message.channel.send(successEmbed)
+                                } 
                             })
                         })
-                    })
+                    }
                 })
             })
         })
